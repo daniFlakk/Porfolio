1b7eb0f94c00092dbeec164a762e2ac809d5ee26
Daniel Ortega <dsantiagortega@hotmail.com>
2021-11-24 18:52:25 -0500

    Primer commit

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+console.log("Script loaded!")
+var form1 = document.getElementById("form");
+form1.addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log("submit");
+    var nameValue = document.getElementById("name").value;
+    var Lastname = document.getElementById("Lastname").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var address = document.getElementById("address").value;
+    var city = document.getElementById("City").value;
+    var born = document.getElementById("born").value;
+    var height = document.getElementById("height").value;
+    var bio = document.getElementById("bio").value;
+    var password = document.getElementById("password").value;
+    console.log(nameValue);
+    document.getElementById("textchange").textContent = nameValue + " " + Lastname + " " + email + " " + phone + " " + address + " " + city + " "
+        + born + " " + height + " " + bio + " " + password;
+});
